37b443f081a21e868c102ba554e7ec0c5cb8073d
IMLaing <ianmlaing@gmail.com>
2016-06-28 18:58:01 -0400

    added previousGuess and newGame button

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+	'use strict';
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -15,13 +16,53 @@ $(document).ready(function(){
 });
 
 // submit a guess (WORKING)
-var gameAnswer = '';
-var totalGuess = 0;
+var $gameAnswer = '';
+var $totalGuess = 0;
+var $previousGuess = '';
+
+var setGuessCount = function (totalGuess){
+document.getElementById('count').innerHTML = totalGuess;
+};
+var newGame = function(){
+	$gameAnswer = Math.floor(Math.random()*100)+1;
+	console.log($gameAnswer);
+	//clear all other contents
+	$('#guessList li').remove();
+	setGuessCount(0);
+};
+var guessTemperature = function(temp){
+	document.getElementById('feedback').innerHTML = temp;
+};
+
+
+$("a.new").click(function(){
+  		newGame();
+  	});
+
+var guessMade = function($userGuess){
+  if ($userGuess == $gameAnswer) {
+    console.log('match on answer');
+    document.getElementById('feedback').innerHTML = "!!!winner!!!";
+  } else {
+    console.log('no match on answer');
+    $('#guessList').append("<li>" + $userGuess + "</li>");
+    $totalGuess++;
+    console.log($totalGuess);
+    setGuessCount($totalGuess); //adds one to guess total and then modifies innerHTML REMEMBER NO # WHEN USING getElementById('')
+    if($userGuess > $gameAnswer + 10 || $userGuess < $gameAnswer - 10){
+      console.log('cold!');
+      guessTemperature('cold!');
+    } else {
+      console.log('hot!');
+      guessTemperature('hot!');
+    }
+  }
+};
 
 $('form').submit(function(event) {
     event.preventDefault();
     var $userGuess = $('#userGuess').val();
-    if (isNaN($userGuess) || $userGuess == '') {
+    if (isNaN($userGuess)) {
       console.log('alert trigged because no input to $userGuess');
       alert('This is not a number');
     } else if($userGuess > 100 || $userGuess < 1){
@@ -29,33 +70,22 @@ $('form').submit(function(event) {
     	alert('the number must be between 1-100');
     } else { 
       console.log('made a guess');
-      	if ($userGuess == gameAnswer) {
-      		console.log('match on answer');
-      		document.getElementById('feedback').innerHTML = "!!!winner!!!";
-      	} else {
-      		console.log('no match on answer');
-      $('#guessList').append("<li>" + $userGuess + "</li>");
-      totalGuess++;
-      console.log(totalGuess);
-      document.getElementById('count').innerHTML = totalGuess; //adds one to guess total and then modifies innerHTML REMEMBER NO # WHEN USING getElementById('')
-  			if($userGuess > gameAnswer + 10 || $userGuess < gameAnswer - 10){
-  				console.log('cold!');
-  				document.getElementById('feedback').innerHTML = "cold!";
-  			} else {
-  				console.log('hot!');
-  				document.getElementById('feedback').innerHTML = "hot!";
-  			}
-  		};
-    };
-    
+      $previousGuess = $userGuess;
+      guessMade($userGuess);
+      console.log($previousGuess);
+    }
     console.log('clear submit field');
     $('#userGuess').val('');
   
   });
 
-var newGame = function(){
-	gameAnswer = Math.floor(Math.random()*100)+1;
-	console.log(gameAnswer);
-};
 
 newGame();
+
+if ( Math.abs($userGuess - $gameAnswer) < Math.abs($previousGuess - $gameAnswer)){
+	console.log('hotter?!?!');
+	guessTemperature('hotter');
+	} else {
+		console.log('colder');
+		guessTemperature('colder');
+	};
