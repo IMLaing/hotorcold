7afffbc03ebd2313b3e8340a529ae525b1add1cd
IMLaing <ianmlaing@gmail.com>
2016-06-27 22:15:11 -0400

    one giant function, needs to be modulated

--- js/app.js
+++ js/app.js
@@ -15,6 +15,8 @@ $(document).ready(function(){
 });
 
 // submit a guess (WORKING)
+var gameAnswer = '';
+var totalGuess = 0;
 
 $('form').submit(function(event) {
     event.preventDefault();
@@ -22,16 +24,37 @@ $('form').submit(function(event) {
     if (isNaN($userGuess) || $userGuess == '') {
       console.log('alert trigged because no input to $userGuess');
       alert('This is not a number');
-    } else {
+    } else if($userGuess > 100 || $userGuess < 1){
+    	console.log(' $userGuess is outside the parameters of the game');
+    	alert('the number must be between 1-100');
+    } else { 
       console.log('made a guess');
+      	if ($userGuess == gameAnswer) {
+      		console.log('match on answer');
+      		document.getElementById('feedback').innerHTML = "!!!winner!!!";
+      	} else {
+      		console.log('no match on answer');
       $('#guessList').append("<li>" + $userGuess + "</li>");
+      totalGuess++;
+      console.log(totalGuess);
+      document.getElementById('count').innerHTML = totalGuess; //adds one to guess total and then modifies innerHTML REMEMBER NO # WHEN USING getElementById('')
+  			if($userGuess > gameAnswer + 10 || $userGuess < gameAnswer - 10){
+  				console.log('cold!');
+  				document.getElementById('feedback').innerHTML = "cold!";
+  			} else {
+  				console.log('hot!');
+  				document.getElementById('feedback').innerHTML = "hot!";
+  			}
+  		};
     };
+    
     console.log('clear submit field');
     $('#userGuess').val('');
+  
   });
 
 var newGame = function(){
-	var gameAnswer = Math.floor(Math.random()*100)+1;
+	gameAnswer = Math.floor(Math.random()*100)+1;
 	console.log(gameAnswer);
 };
 
